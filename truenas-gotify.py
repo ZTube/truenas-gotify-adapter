ddd240df36ca156744cb1e8efa775683d13f861a
ZTube <ztube@users.noreply.github.com>
2025-01-01 23:14:50 +0100

    Add debug output for incorrect token

--- truenas-gotify.py
+++ truenas-gotify.py
@@ -12,6 +12,7 @@ GOTIFY_BASEURL = os.environ.get("GOTIFY_URL")
 GOTIFY_TOKEN = os.environ.get("GOTIFY_TOKEN")
 
 # The ip address the service should listen on
+# Defaults to localhost for security reasons
 LISTEN_HOST = os.environ.get("LISTEN_HOST", "127.0.0.1")
 PORT = 31662
 
@@ -24,20 +25,29 @@ routes = web.RouteTableDef()
 async def on_message(request):
     content = await request.json()
     # The content of the alert message
-    message = content["text"]
-    print("===== Alert =====")
+    message = content["text"].strip()
+    print("=========== NOTIFICATION ===========")
     print(message)
+    print("========= NOTIFICATION END =========")
 
     # Forward the alert to gotify
     gotify_resp = await send_gotify_message(message, GOTIFY_TOKEN)
+
+    # Check for http reponse status code 'success'
+    if gotify_resp.status == 200:
+        print(">> Forwarded successfully\n")
+    elif gotify_resp.status in [400, 401, 403]:
+        print(f">> Unauthorized! Token GOTIFY_TOKEN='{GOTIFY_TOKEN}' is incorrect\n")
+    else:
+        print(f">> Unknown error while forwarding to gotify. Error Code {gotify_resp.status}")
 
     # Return the gotify status code to truenas
     return web.Response(status=gotify_resp.status)
 
 # Send an arbitrary alert to gotify
 async def send_gotify_message(message, token, title=None, priority=None):
-    # URL parameters
-    params = {"token": token}
+    # Set token through header
+    headers = {"X-Gotify-Key": token}
     # POST body
     json = {"message": message}
 
@@ -48,12 +58,11 @@ async def send_gotify_message(message, token, title=None, priority=None):
         json["priority"] = priority
 
     async with ClientSession() as session:
-        async with session.post(GOTIFY_BASEURL, params=params, json=json) as resp:
+        async with session.post(GOTIFY_BASEURL, headers=headers, json=json) as resp:
             return resp
 
-
 if __name__ == "__main__":
-    # Check if env variable is set
+    # Check if env variables are set
     if GOTIFY_BASEURL == None:
         sys.exit("Set Gotify Endpoint via 'GOTIFY_URL=http[s]://{host}:{port}/'!")
     if GOTIFY_TOKEN == None:
@@ -65,8 +74,7 @@ if __name__ == "__main__":
             GOTIFY_BASEURL += "/"
         GOTIFY_BASEURL += "message"
 
-
-    # Listen on default port
+    # Listen
     app = web.Application()
     app.add_routes(routes)
     web.run_app(app, host=LISTEN_HOST, port=PORT)
